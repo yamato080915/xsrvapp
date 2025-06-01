2020a167c1da7972a5c61cb6baeeb1aa621750a4
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-06-01 19:11:08 +0900

    add errorhandler

--- www.py
+++ www.py
@@ -1,5 +1,6 @@
 from app import app, login_manager
 from models import User
+from flask import render_template
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -22,3 +23,23 @@ login_manager.blueprint_login_views = {
 	"steam": "account.login",
     "math": "account.login"
 }
+
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template("error/400.html"), 400
+
+@app.errorhandler(401)
+def unauthorized(e):
+    return render_template("error/401.html"), 401
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template("error/403.html"), 403
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("error/404.html"), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("error/500.html"), 500

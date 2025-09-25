7ae19f2d4727f94775d9b6a78b58561006d1306d
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-09-25 23:13:24 +0900

    stage-2/pacific.json

--- controllers/valorant/__init__.py
+++ controllers/valorant/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, make_response, send_file
+from flask import Blueprint, render_template, make_response, send_file, abort
 from myfunc import url_for
 import json, os
 
@@ -30,22 +30,34 @@ def serve_icons(team):
 		response.headers["Cache-Control"] = 'public, max-age=31536000'
 		return response
 
+def load_teams():
+	with open(f'controllers{url_for("valorant.static", filename="vct/teams.json")}', encoding="utf-8") as f:
+		return json.load(f)
+
+@valorant.route("/vct/<int:year>/stage-2/<region>")
+def stage2(year, region:str):
+	if not os.path.isfile(f"controllers/valorant/statics/vct/{year}/stage-2/{region.lower()}.json"):
+		abort(404)
+	with open(f"controllers/valorant/statics/vct/{year}/stage-2/{region.lower()}.json", encoding="utf-8") as f:
+		data = json.load(f)
+	return render_template("valorant/stage-2.html", data=data, teams=load_teams())
+
 @valorant.route("/vct/<int:year>/champions")
 def champions(year):
+	if not os.path.isdir(f"controllers/valorant/statics/vct/{year}"):
+		abort(404)
 	with open(f'controllers{url_for("valorant.static", filename=f"vct/{year}/champions.json")}', encoding="utf-8") as f:
 		data = json.load(f)
-	with open(f'controllers{url_for("valorant.static", filename="vct/teams.json")}', encoding="utf-8") as f:
-		teams = json.load(f)
 	name = f"valorant champions {data['champions']}".upper()
 	data = data["tournament"][f"champions {data['champions']}"]
-	return render_template("valorant/champions.html", name=name, tournament=data, teams=teams)
+	return render_template("valorant/champions.html", name=name, tournament=data, teams=load_teams())
 
 @valorant.route("/vct/<int:year>/champions/pickem")
 def pickem(year):
+	if not os.path.isfile(f"controllers/valorant/statics/vct/{year}/pick'em.json"):
+		abort(404)
 	with open(f'controllers{url_for("valorant.static", filename=f"vct/{year}/pick\'em.json")}', encoding="utf-8") as f:
 		data = json.load(f)
-	with open(f'controllers{url_for("valorant.static", filename="vct/teams.json")}', encoding="utf-8") as f:
-		teams = json.load(f)
 	name = f"valorant champions {data['champions']}".upper()
 	data = data["tournament"][f"champions {data['champions']}"]
-	return render_template("valorant/champions.html", name=name, tournament=data, teams=teams)
+	return render_template("valorant/champions.html", name=name, tournament=data, teams=load_teams())
